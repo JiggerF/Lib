508ffc55c037e43c5173c9d8026e456834853a72
jigger <jigger.fantonial@myob.com>
2017-03-19 19:12:24 +1100

    test jenkins integration

---
+++ UnitTestProject1/UnitTestProject1/Common/Driver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Myob.IdentityServer.AuthorizationServerWebApplication.UITests.Common
+{
+    public class Driver
+    {
+        public static IWebDriver Instance;
+        protected static readonly ImageFormat ScreenshotImageFormat = ImageFormat.Png;
+
+
+        public Driver()
+        {
+
+        }
+
+        public static void Init()
+        {
+            try
+            {
+                Instance = new ChromeDriver();
+                Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+                Instance.Manage().Window.Maximize();
+                Console.WriteLine("WebDriver initialized...");
+            }
+            catch (WebDriverException e)
+            {
+                Console.WriteLine("Error initializing webdriver!");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Close()
+        {
+            Driver.Instance.Close();
+            Driver.Instance.Quit();
+
+            Console.WriteLine("Closing webdriver instance...");
+        }
+
+        public static void TakeScreenShot()
+        {
+            var testName = TestContext.CurrentContext.Test.Name;
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd-hhmm-ss");
+
+            Screenshot ss = ((ITakesScreenshot)Driver.Instance).GetScreenshot();
+            var base64 = ss.AsBase64EncodedString;
+            ss.SaveAsFile(testName + "_" + timestamp + ".png", ScreenshotImageFormat);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/Tests/Sample1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Myob.IdentityServer.AuthorizationServerWebApplication.UITests.Common;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class Sample1
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine($"Starting Test....... ");
+        }
+       
+
+        [Test]
+        [Category("Functional")]
+        public void VisitFacebook()
+        {
+            Driver.Init();
+            Console.WriteLine($"Starting Method VisitFacebook ....... ");
+            Driver.Instance.Navigate().GoToUrl("http://www.facebook.com");
+            Driver.Instance.FindElement(By.XPath(".//button[contains(text(),'Create an account')]")).Click();
+            Driver.Instance.FindElement(By.Name("firstname")).SendKeys("John");
+            Driver.Instance.FindElement(By.Name("lastname")).SendKeys("Alexander");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Instance.Close();
+            Driver.Instance.Quit();
+        }
+    }
+}
